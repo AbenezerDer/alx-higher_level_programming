d758e24a1616bc27253f1bade92bc50fca059377
AbenezerDer <dabenu2012@gmail.com>
2022-12-05 13:16:32 -0500

    hot fix

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print_list_integer = __import__('0-print_list_integer').print_list_integer
+def print_list_integer(my_list=[]):
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
