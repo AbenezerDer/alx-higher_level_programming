aebb35d475cdda63eb329494e850b97add806e04
AbenezerDer <dabenu2012@gmail.com>
2022-12-19 12:37:00 -0500

    safe print list intigers

---
+++ 2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    real = 0
+
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            real += 1
+        except (ValueError, TypeError):
+            pass
+
+    print()
+    return real
