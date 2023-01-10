782a4a1ccdc99448e508bd344f307edd0d1a5f5c
Abenezer <dabenu2012@gmail.com>
2023-01-10 14:49:09 +0300

    Update 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
-""" Module that defines the class Student
-"""
+"""This module defines a class Student"""
 
 
 class Student:
-    """ Class to create student instances """
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """ Special method to initialize """
+        """Initializes a new Student
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """ Method that returns directory description """
-        return self.__dict__.copy()
+    def to_json(self, attrs=None):
+        """Gets a dictionary representation of the Student.
+        If attrs is a list of strings, represents only those attributes
+        included in the list
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
