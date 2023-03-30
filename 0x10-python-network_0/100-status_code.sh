0b8ea4fbdb7a202f39fe941ddca6b4686464615c
Abenezer <dabenu2012@gmail.com>
2023-03-30 15:02:44 -0400

    Update 100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a req to $1 URL, display response status code only
-curl -s -o /dev/null -w "%{http_code}" "$1"
+# Script that sends a request to a URL passed as an argument, and displays only the status code of the response
+curl -so /dev/null -w "%{http_code}" "$1"
