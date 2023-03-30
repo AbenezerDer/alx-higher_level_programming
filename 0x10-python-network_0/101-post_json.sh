5ff66f1c83407266701e6f6cd0ffd41a43e2dfcd
Abenezer <dabenu2012@gmail.com>
2023-03-30 15:05:52 -0400

    Update 101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a JSON POST req to URL $!, display response body
-curl -sL -H "content-type:application/json"  -d @"$2" -X POST "$1"
+# Script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
+curl -s "$1" -d "@$2" -X POST -H "Content-Type: application/json"
