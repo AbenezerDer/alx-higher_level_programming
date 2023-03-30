062d3fe47a2f12726514a0549e44097898e32f88
Abenezer <dabenu2012@gmail.com>
2023-03-30 15:08:13 -0400

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# makes a req to 0.0.0.0:5000/catch_me for a response "You got me!"
-curl -sX PUT -L -d "user_id=98" --header "origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+# Script that makes a request to causes an specific response
+curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin:HolbertonSchool"
