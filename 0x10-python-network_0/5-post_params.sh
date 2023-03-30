ca1f27b1b6997f22219eafbb29862194b3e34be3
Abenezer <dabenu2012@gmail.com>
2023-03-30 14:41:11 -0400

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#curl sends POST req to URL, display response body
-curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+# Script that sends a POST request and displays the body response
+curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
