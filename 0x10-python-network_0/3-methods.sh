251d97ed2c3ae1513976091cc0d7830f24a2d084
Abenezer <dabenu2012@gmail.com>
2023-03-30 14:32:57 -0400

    Update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# curl to display all HTTP methods the server accepts
-curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
+# Script that takes an URL and shows the Allowed OPTIONS
+curl -sI -X OPTIONS "$1" | grep "Allow:" | cut -d " " -f 2-
