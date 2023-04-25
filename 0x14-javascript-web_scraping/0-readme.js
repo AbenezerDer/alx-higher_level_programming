4d7c3b97ed0584e2194e19872bd6972b6cf8fc5b
Abenezer <dabenu2012@gmail.com>
2023-04-25 16:15:42 -0400

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-fs.readFile(process.argv[2], 'utf-8', function (err, result) {
-  if (err) {
+fs.readFile(process.argv[2], 'utf8', function (err, contents) {
+  if (contents === undefined) {
     console.log(err);
   } else {
-    console.log(result);
+    console.log(contents);
   }
 });
