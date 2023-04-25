76d374917e6460b664c9020e8d844ec101554966
AbenezerDer <dabenu2012@gmail.com>
2023-04-25 16:11:15 -0400

    javascript-web_scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (err, contents) {
-  if (contents === undefined) {
+
+fs.readFile(process.argv[2], 'utf-8', function (err, result) {
+  if (err) {
     console.log(err);
   } else {
-    console.log(contents);
+    console.log(result);
   }
 });
