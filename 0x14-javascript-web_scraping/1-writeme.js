fda79a5f325e365e64d1a7a3494b1c9231259ef0
Abenezer <dabenu2012@gmail.com>
2023-04-25 15:44:20 -0400

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-const file = process.argv[2];
-const string = process.argv[3];
-
-fs.writeFile(file, string, 'utf-8', function (err) {
-  if (err) console.log(err);
-});
+fs.writeFileSync(process.argv[2], process.argv[3]);
